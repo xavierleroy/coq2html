5c799e5ac721fc142fb260fdc4e89e61de6a1ab5
Xavier Leroy <xavier.leroy@college-de-france.fr>
2024-01-10 18:36:01 +0100

    Add explicit interface for resources.ml
    It's good style.

---
+++ resources.mli
@@ -0,0 +1,19 @@
+(* *********************************************************************)
+(*                                                                     *)
+(*              The Coq2HTML documentation generator                   *)
+(*                                                                     *)
+(*          Xavier Leroy, Collège de France and INRIA Paris            *)
+(*                                                                     *)
+(*  Copyright Institut National de Recherche en Informatique et en     *)
+(*  Automatique.  All rights reserved.  This file is distributed       *)
+(*  under the terms of the GNU General Public License as published by  *)
+(*  the Free Software Foundation, either version 2 of the License, or  *)
+(*  (at your option) any later version.                                *)
+(*                                                                     *)
+(* *********************************************************************)
+
+val header : string
+val footer : string
+val css : string
+val js : string
+val redirect : string
